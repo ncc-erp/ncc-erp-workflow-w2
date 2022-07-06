c0fab89727ed29c72290e6e2f684e1c55cd57fbd
Ha Nguyen Ngan <ha.nguyen@ncc.asia>
2022-07-06 09:57:19 +0700

    Create new account after external login

--- src/W2.Web/Pages/Account/Login.cshtml.cs
+++ src/W2.Web/Pages/Account/Login.cshtml.cs
@@ -3,11 +3,17 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Volo.Abp;
 using Volo.Abp.Account.Web;
 using Volo.Abp.Account.Web.Pages.Account;
+using Volo.Abp.Identity;
+using Volo.Abp.Identity.AspNetCore;
 using Volo.Abp.Security.Claims;
 using IdentityUser = Volo.Abp.Identity.IdentityUser;
 
@@ -19,9 +25,119 @@ namespace W2.Web.Pages.Account
         {
         }
 
-        public override Task<IActionResult> OnGetExternalLoginCallbackAsync(string returnUrl = "", string returnUrlHash = "", string remoteError = null)
+        public override async Task<IActionResult> OnGetExternalLoginCallbackAsync(string returnUrl = "", string returnUrlHash = "", string remoteError = null)
         {
-            return base.OnGetExternalLoginCallbackAsync(returnUrl, returnUrlHash, remoteError);
+            if (remoteError != null)
+            {
+                Logger.LogWarning($"External login callback error: {remoteError}");
+                return RedirectToPage("./Login");
+            }
+
+            await IdentityOptions.SetAsync();
+
+            var loginInfo = await SignInManager.GetExternalLoginInfoAsync();
+            if (loginInfo == null)
+            {
+                Logger.LogWarning("External login info is not available");
+                return RedirectToPage("./Login");
+            }
+
+            var result = await SignInManager.ExternalLoginSignInAsync(
+                loginInfo.LoginProvider,
+                loginInfo.ProviderKey,
+                isPersistent: false,
+                bypassTwoFactor: true
+            );
+
+            if (!result.Succeeded)
+            {
+                await IdentitySecurityLogManager.SaveAsync(new IdentitySecurityLogContext()
+                {
+                    Identity = IdentitySecurityLogIdentityConsts.IdentityExternal,
+                    Action = "Login" + result
+                });
+            }
+
+            if (result.IsLockedOut)
+            {
+                Logger.LogWarning($"External login callback error: user is locked out!");
+                throw new UserFriendlyException("Cannot proceed because user is locked out!");
+            }
+
+            if (result.IsNotAllowed)
+            {
+                Logger.LogWarning($"External login callback error: user is not allowed!");
+                throw new UserFriendlyException("Cannot proceed because user is not allowed!");
+            }
+
+            if (result.Succeeded)
+            {
+                return RedirectSafely(returnUrl, returnUrlHash);
+            }
+
+            var email = loginInfo.Principal.FindFirstValue(AbpClaimTypes.Email);
+            if (email.IsNullOrWhiteSpace())
+            {
+                var identityEmail = loginInfo.Principal.Identities.FirstOrDefault()?.FindFirst(ClaimTypes.Email)?.Value;
+                if (identityEmail.IsNullOrWhiteSpace())
+                {
+                    throw new UserFriendlyException("Email not found");
+                }
+
+                await RegisterExternalUserAsync(loginInfo, identityEmail);
+
+                return Redirect(ReturnUrl ?? "~/");
+            }
+
+            var user = await UserManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                user = await CreateExternalUserAsync(loginInfo);
+            }
+            else
+            {
+                if (await UserManager.FindByLoginAsync(loginInfo.LoginProvider, loginInfo.ProviderKey) == null)
+                {
+                    CheckIdentityErrors(await UserManager.AddLoginAsync(user, loginInfo));
+                }
+            }
+
+            await SignInManager.SignInAsync(user, false);
+
+            await IdentitySecurityLogManager.SaveAsync(new IdentitySecurityLogContext()
+            {
+                Identity = IdentitySecurityLogIdentityConsts.IdentityExternal,
+                Action = result.ToIdentitySecurityLogAction(),
+                UserName = user.Name
+            });
+
+            return RedirectSafely(returnUrl, returnUrlHash);
+        }
+
+        private async Task RegisterExternalUserAsync(ExternalLoginInfo externalLoginInfo, string emailAddress)
+        {
+            await IdentityOptions.SetAsync();
+
+            var user = new IdentityUser(GuidGenerator.Create(), emailAddress, emailAddress, CurrentTenant.Id);
+
+            (await UserManager.CreateAsync(user)).CheckErrors();
+            (await UserManager.AddDefaultRolesAsync(user)).CheckErrors();
+
+            var userLoginAlreadyExists = user.Logins.Any(x =>
+                x.TenantId == user.TenantId &&
+                x.LoginProvider == externalLoginInfo.LoginProvider &&
+                x.ProviderKey == externalLoginInfo.ProviderKey);
+
+            if (!userLoginAlreadyExists)
+            {
+                (await UserManager.AddLoginAsync(user, new UserLoginInfo(
+                    externalLoginInfo.LoginProvider,
+                    externalLoginInfo.ProviderKey,
+                    externalLoginInfo.ProviderDisplayName
+                ))).CheckErrors();
+            }
+
+            await SignInManager.SignInAsync(user, isPersistent: true);
         }
     }
 }
