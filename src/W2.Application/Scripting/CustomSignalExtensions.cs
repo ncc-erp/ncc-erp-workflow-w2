316675e3f9154de26cd52a6f582d13905f9831e3
Ha Nguyen <ha.nguyenngan311@gmail.com>
2022-09-12 16:06:59 +0700

    Add reject reason form

---
+++ src/W2.Application/Scripting/CustomSignalExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Elsa.Services;
+using Elsa.Services.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace W2.Signals
+{
+    public static class CustomSignalExtensions
+    {
+        public static string GenerateSignalTokenWithForm(this ActivityExecutionContext context, string signal, string[] requiredInputs)
+        {
+            string id = context.WorkflowExecutionContext.WorkflowInstance.Id;
+            var payload = new SignalModelDto
+            {
+                Name = signal,
+                WorkflowInstanceId = id,
+                RequiredInputs = requiredInputs.ToList()
+            };
+            ITokenService service = context.GetService<ITokenService>();
+            return service.CreateToken(payload);
+        }
+    }
+}
