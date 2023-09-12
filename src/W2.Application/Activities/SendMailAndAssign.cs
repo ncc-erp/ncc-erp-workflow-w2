0a6849bcdc285ee98c63c66f641200ea1dcdff9d
longvodinhhoang <long.vodinhhoang@ncc.asia>
2023-09-12 17:05:30 +0700

    modified elsa workflow

--- src/W2.Application/Activities/SendMailAndAssign.cs
+++ src/W2.Application/Activities/SendMailAndAssign.cs
@@ -2,14 +2,18 @@
 using Elsa.Activities.Email;
 using Elsa.Activities.Email.Options;
 using Elsa.Activities.Email.Services;
+using Elsa.Activities.Signaling.Models;
 using Elsa.ActivityResults;
 using Elsa.Attributes;
 using Elsa.Serialization;
+using Elsa.Services;
 using Elsa.Services.Models;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using W2.Signals;
 using W2.Tasks;
@@ -24,17 +28,25 @@ namespace W2.Activities
     public class SendMailAndAssign : SendEmail
     {
         private ITaskAppService _taskAppService;
+        private readonly ITokenService _tokenService;
         public SendMailAndAssign(ISmtpService smtpService, 
             IOptions<SmtpOptions> options, 
             IHttpClientFactory httpClientFactory,
             ITaskAppService taskAppService,
-            IContentSerializer contentSerializer) 
+            IContentSerializer contentSerializer,
+            ITokenService tokenService) 
             : base(smtpService, options, httpClientFactory, contentSerializer)
         {
             _taskAppService = taskAppService;
+            _tokenService = tokenService;
         }
 
         public new string From => string.Empty;
+        [ActivityInput(Hint = "The approved signal")]
+        public string ApproveSignal { get; set; }
+
+        [ActivityInput(Hint = "The reject signal")]
+        public string RejectSignal { get; set; }
 
         protected async override ValueTask<IActivityExecutionResult> OnExecuteAsync(ActivityExecutionContext context)
         {
@@ -44,13 +56,15 @@ namespace W2.Activities
             }
 
             var currentUser = context.GetRequestUserVariable();
+
             foreach(var email in To)
             {
                 if (email != null)
                 {
-                    await _taskAppService.assignTask(email, (Guid)currentUser.Id, context.WorkflowInstance.Id);
+                    await _taskAppService.assignTask(email, (Guid)currentUser.Id, context.WorkflowInstance.Id, Subject, ApproveSignal.Trim(), RejectSignal.Trim());
                 }
             }
+
             return await base.OnExecuteAsync(context);
         }
     }
--- src/W2.Domain/Tasks/W2Task.cs
+++ src/W2.Domain/Tasks/W2Task.cs
@@ -7,13 +7,14 @@ namespace W2.Tasks
 {
     public class W2Task : CreationAuditedEntity<Guid>, IMultiTenant
     {
-        //public string WorkflowInstanceId { get; set; } = default!;
-        //public Dictionary<string, string> Input { get; set; }
         public string WorkflowInstanceId { get; set; } = default!;
         public string Email { get; set; }
         public Dictionary<string, string> Data { get; set; }
         public Guid? TenantId { get; set; }
         public Guid Author { get; set; }
         public W2TaskStatus Status { get; set; }
+        public string Name { get; set; } // Task name
+        public string ApproveSignal { get; set; } // Task when approving
+        public string RejectSignal { get; set; } // Task when rejecting
     }
 }
