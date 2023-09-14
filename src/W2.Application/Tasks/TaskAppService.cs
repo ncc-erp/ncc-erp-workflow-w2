a29d66ff4da1cf1d44d79cf2c80b0858f6ff3d77
longvodinhhoang <long.vodinhhoang@ncc.asia>
2023-09-14 16:46:27 +0700

    51097: fix task name

--- src/W2.Application/Tasks/TaskAppService.cs
+++ src/W2.Application/Tasks/TaskAppService.cs
@@ -1,6 +1,7 @@
 ﻿using Elsa.Activities.Http.Events;
 using Elsa.Activities.Signaling.Models;
 using Elsa.Activities.Signaling.Services;
+using Elsa.Models;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Open.Linq.AsyncExtensions;
@@ -13,7 +14,10 @@ using Volo.Abp;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Users;
+using Elsa.Persistence.Specifications;
+using Elsa.Persistence.Specifications.WorkflowInstances;
 using Elsa.Persistence;
+using W2.Specifications;
 using Elsa;
 using Newtonsoft.Json;
 
@@ -27,25 +31,30 @@ namespace W2.Tasks
         private readonly IMediator _mediator;
         private readonly ICurrentUser _currentUser;
         private readonly IWorkflowInstanceStore _workflowInstanceStore;
+        private readonly IWorkflowDefinitionStore _workflowDefinitionStore;
 
         public TaskAppService(IRepository<W2Task, Guid> taskRepository, 
             ISignaler signaler, 
             IMediator mediator, 
             ICurrentUser currentUser,
-            IWorkflowInstanceStore workflowInstanceStore)
+            IWorkflowInstanceStore workflowInstanceStore,
+            IWorkflowDefinitionStore workflowDefinitionStore)
         {
             _signaler = signaler;
             _taskRepository = taskRepository;
             _mediator = mediator;
             _currentUser = currentUser;
             _workflowInstanceStore = workflowInstanceStore;
+            _workflowDefinitionStore = workflowDefinitionStore;
         }
 
         //[Authorize(W2Permissions.WorkflowManagementSettingsSocialLoginSettings)]
-        public async Task assignTask(string email, Guid userId, string workflowInstanceId, string Name, string ApproveSignal, string RejectSignal)
+        public async Task assignTask(string email, Guid userId, string workflowInstanceId, string ApproveSignal, string RejectSignal)
         {
 
             var workflowInstance = await _workflowInstanceStore.FindByIdAsync(workflowInstanceId);
+            var workflowDefinitions = (await _workflowDefinitionStore.FindManyAsync(
+                new ListAllWorkflowDefinitionsSpecification(CurrentTenantStrId, new string[] { workflowInstance.DefinitionId }))).ToList();
 
             await _taskRepository.InsertAsync(new W2Task
             {
@@ -55,7 +64,7 @@ namespace W2.Tasks
                 WorkflowInstanceId = workflowInstanceId,
                 WorkflowDefinitionId = workflowInstance.DefinitionId,
                 Status = W2TaskStatus.Pending,
-                Name = Name,
+                Name = workflowDefinitions.First().Name,
                 ApproveSignal = ApproveSignal, 
                 RejectSignal = RejectSignal 
             });
