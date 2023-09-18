b44fedcf1a3d5f9fc589090dacbd6add56007157
thiendangncc <thien.dang@ncc.asia>
2023-09-18 21:04:42 +0700

    update github build

--- parse_env.sh
+++ parse_env.sh
@@ -17,4 +17,5 @@ sed "s/SES_PASS/$SES_PASS/g" -i appsettings.json
 sed "s/GG_CLIENT/$GG_CLIENT/g" -i appsettings.json
 sed "s/GG_SECRET/$GG_SECRET/g" -i appsettings.json
 sed "s/TALENT_DF/$TALENT_DF/g" -i appsettings.json
+sed "s/WFH_DEFINITIONS_ID/$WFH_DEFINITIONS_ID/g" -i appsettings.json
 
