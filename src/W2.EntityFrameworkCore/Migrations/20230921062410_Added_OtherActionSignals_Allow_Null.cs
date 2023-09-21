0dd6aa9672134e02cec0f77aee5380276c9dee96
longvodinhhoang <long.vodinhhoang@ncc.asia>
2023-09-21 13:30:28 +0700

    ađ migrate file

---
+++ src/W2.EntityFrameworkCore/Migrations/20230921062410_Added_OtherActionSignals_Allow_Null.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace W2.Migrations
+{
+    public partial class Added_OtherActionSignals_Allow_Null : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
