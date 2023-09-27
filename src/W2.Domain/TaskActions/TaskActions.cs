c9c4ef32dfa303d73aad2e3f72f89fa657c03ea5
longvodinhhoang <long.vodinhhoang@ncc.asia>
2023-09-27 17:12:07 +0700

    52418: add W2TaskAction Entity

---
+++ src/W2.Domain/TaskActions/TaskActions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Entities.Auditing;
+using W2.Tasks;
+
+namespace W2.TaskActions
+{
+    public class W2TaskActions : BasicAggregateRoot<Guid>
+    {
+        public string OtherActionSignal { get; set; } // List of other action signals for Task
+        public string TaskId { get; set; }
+        public string UpdatedBy { get; set; } // Task description
+        public W2TaskActionsStatus Status { get; set; }
+    }
+}
