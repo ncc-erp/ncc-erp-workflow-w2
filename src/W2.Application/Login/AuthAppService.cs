295095aee67ed80bf693fcfafce4128d89ba7304
hangncc <hang.caothithuy@ncc.asia>
2023-09-29 17:31:39 +0700

    52688: Login with username/password

---
+++ src/W2.Application/Login/AuthAppService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Identity;
+using W2.ExternalResources;
+
+namespace W2.Login
+{
+    public class AuthAppService : W2AppService, IAuthAppService
+    {
+        private readonly IdentityUserManager _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthAppService(
+            IdentityUserManager userManager,
+            IConfiguration configuration
+            )   
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<AuthUser> LoginAccount(AuthDto authDto)
+        {
+            var user = await _userManager.FindByNameAsync(authDto.userNameOrEmailAddress);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, authDto.password))
+            {
+                var token = GenerateJwtTokenForUser(user);
+                return new AuthUser { Token = token };
+            }
+
+            throw new UserFriendlyException("Invalid username or password.");
+        }
+
+        private string GenerateJwtTokenForUser(Volo.Abp.Identity.IdentityUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            };
+
+            var roles = _userManager.GetRolesAsync(user).Result;
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]));
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+
+    }
+}
