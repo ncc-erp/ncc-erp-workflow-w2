5816237d8f3a983cbe253eabfa2b0c1864692f49
diem.nguyen <diem.nguyenthihong@ncc.asia>
2024-09-20 09:58:17 +0700

    filter not including not published

--- src/W2.Application.Contracts/WorkflowDefinitions/IWorkflowDefinitionAppService.cs
+++ src/W2.Application.Contracts/WorkflowDefinitions/IWorkflowDefinitionAppService.cs
@@ -8,7 +8,7 @@ namespace W2.WorkflowDefinitions
     public interface IWorkflowDefinitionAppService : IApplicationService
     {
         Task<WorkflowDefinitionSummaryDto> GetByDefinitionIdAsync(string definitionId);
-        Task<PagedResultDto<WorkflowDefinitionSummaryDto>> ListAllAsync();
+        Task<PagedResultDto<WorkflowDefinitionSummaryDto>> ListAllAsync(bool? isPublish = false);
         Task SaveWorkflowInputDefinitionAsync(WorkflowCustomInputDefinitionDto input);
         Task<string> CreateWorkflowDefinitionAsync(CreateWorkflowDefinitionDto input);
         Task DeleteAsync(string id);
