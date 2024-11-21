4e1fffb314c86bf7b019126caccc66f8bc38412b
huynguyenanh1 <huy.nguyenanh@ncc.asia>
2024-11-21 11:12:47 +0700

    fix file migation

--- src/W2.EntityFrameworkCore/Migrations/20241058023402_W2PermissionData.cs
+++ src/W2.EntityFrameworkCore/Migrations/20241058023402_W2PermissionData.cs
@@ -15,44 +15,48 @@ namespace W2.Migrations
                 IF NOT EXISTS (SELECT 1 FROM ""W2Permissions"") THEN
                 INSERT INTO ""W2Permissions"" (""Id"", ""Name"", ""Code"", ""ParentId"", ""CreatorId"", ""CreationTime"")
                 VALUES 
-                    ('353896e7-9516-4313-a5ce-3b8bec18a279', 'Requests', 'WorkflowInstances', NULL, NULL, '2024-11-19T07:35:52.981453Z'),
-                    ('377ad1fd-b7ef-4849-92d5-b945a9d2ca0f', 'View List Requests', 'WorkflowInstances.View', '353896e7-9516-4313-a5ce-3b8bec18a279', NULL, '2024-11-19T07:35:52.985780Z'),
-                    ('9cc23baf-b5cb-4e4a-874f-defcf806ae20', 'Create Requests', 'WorkflowInstances.Create', '353896e7-9516-4313-a5ce-3b8bec18a279', NULL, '2024-11-19T07:35:52.986039Z'),
-                    ('be869468-5117-4619-9a3c-eb26ddb78970', 'Cancel Requests', 'WorkflowInstances.Cancel', '353896e7-9516-4313-a5ce-3b8bec18a279', NULL, '2024-11-19T07:35:52.986158Z'),
-                    ('9980cba3-072d-4fc9-8576-197597b01ab1', 'View All Requests', 'WorkflowInstances.ViewAll', '353896e7-9516-4313-a5ce-3b8bec18a279', NULL, '2024-11-19T07:35:52.985911Z'),
+                    ('cb3433ea-e362-4181-85e8-25c0b6de4219', 'Requests', 'WorkflowInstances', NULL, NULL, '2024-11-19T07:35:52.981453Z'),
+                    ('7458fe46-1fe0-4091-b6b5-bc2fedc35ca9', 'View List Requests', 'WorkflowInstances.View', 'cb3433ea-e362-4181-85e8-25c0b6de4219', NULL, '2024-11-19T07:35:52.985780Z'),
+                    ('7673eb7f-142d-4f00-8adf-4dccd4132916', 'Create Requests', 'WorkflowInstances.Create', 'cb3433ea-e362-4181-85e8-25c0b6de4219', NULL, '2024-11-19T07:35:52.986039Z'),
+                    ('f3654bcb-e323-4d44-bf01-49e122045ed1', 'Cancel Requests', 'WorkflowInstances.Cancel', 'cb3433ea-e362-4181-85e8-25c0b6de4219', NULL, '2024-11-19T07:35:52.986158Z'),
+                    ('0dc259ca-bf21-4b32-89a3-9de4ef114e18', 'View All Requests', 'WorkflowInstances.ViewAll', 'cb3433ea-e362-4181-85e8-25c0b6de4219', NULL, '2024-11-19T07:35:52.985911Z'),
 
                     -- Children Permissions for Roles
-                    ('6990fbaf-5a00-40cf-a3a1-13b85d1f6099', 'Roles Management', 'Roles', NULL, NULL, '2024-11-19T07:35:52.982008Z'),
-                    ('54e8008c-6d2d-4c00-873f-39c33c3ed705', 'View List Roles', 'Roles.View', '6990fbaf-5a00-40cf-a3a1-13b85d1f6099', NULL, '2024-11-19T07:35:52.987715Z'),
-                    ('8eb0cb9b-7cab-4d38-9163-a9a4cc8ea7e0', 'Update Role', 'Roles.Update', '6990fbaf-5a00-40cf-a3a1-13b85d1f6099', NULL, '2024-11-19T07:35:52.988002Z'),
-                    ('9c41b0c6-fcae-49c0-94d1-146f392fda3f', 'Create Role', 'Roles.Create', '6990fbaf-5a00-40cf-a3a1-13b85d1f6099', NULL, '2024-11-19T07:35:52.987866Z'),
-                    ('e117cd9c-2cde-4ec6-8d51-1fc37b0a282a', 'Delete User On Role', 'Roles.DeleteUserOnRole', '6990fbaf-5a00-40cf-a3a1-13b85d1f6099', NULL, '2024-11-19T07:35:52.987866Z'),
-                    ('e18dd7ad-ecc1-4ef6-9574-efa55f4fcd18', 'Delete Role', 'Roles.Delete', '6990fbaf-5a00-40cf-a3a1-13b85d1f6099', NULL, '2024-11-19T07:35:52.987866Z'),
+                    ('bb367208-d345-4182-8854-57a6cdf1ae9e', 'Roles Management', 'Roles', NULL, NULL, '2024-11-19T07:35:52.982008Z'),
+                    ('54e8008c-6d2d-4c00-873f-39c33c3ed705', 'View List Roles', 'Roles.View', 'bb367208-d345-4182-8854-57a6cdf1ae9e', NULL, '2024-11-19T07:35:52.987715Z'),
+                    ('5dbcbd58-ac15-48a8-86d2-853c96c05359', 'Update Role', 'Roles.Update', 'bb367208-d345-4182-8854-57a6cdf1ae9e', NULL, '2024-11-19T07:35:52.988002Z'),
+                    ('743f4fb1-b57b-4ff8-a1cd-9a9bb16ec5f3', 'Create Role', 'Roles.Create', 'bb367208-d345-4182-8854-57a6cdf1ae9e', NULL, '2024-11-19T07:35:52.987866Z'),
+                    ('e117cd9c-2cde-4ec6-8d51-1fc37b0a282a', 'Delete User On Role', 'Roles.DeleteUserOnRole', 'bb367208-d345-4182-8854-57a6cdf1ae9e', NULL, '2024-11-19T07:35:52.987866Z'),
+                    ('e18dd7ad-ecc1-4ef6-9574-efa55f4fcd18', 'Delete Role', 'Roles.Delete', 'bb367208-d345-4182-8854-57a6cdf1ae9e', NULL, '2024-11-19T07:35:52.987866Z'),
 
                     -- Children Permissions for Tasks
-                    ('881263f8-95d7-4410-88e5-d3873f99b53e', 'Tasks Management', 'Tasks', NULL, NULL, '2024-11-19T07:35:52.981659Z'),
-                    ('034d0ba5-be32-4e84-8ee7-09b7a809bb4c', 'Assign Task', 'Tasks.Assign', '881263f8-95d7-4410-88e5-d3873f99b53e', NULL, '2024-11-19T07:35:52.986700Z'),
-                    ('33fc5382-9f93-41d6-acc7-751e239b7e4f', 'View List Tasks', 'Tasks.View', '881263f8-95d7-4410-88e5-d3873f99b53e', NULL, '2024-11-19T07:35:52.986298Z'),
-                    ('6c131f7f-0295-417d-8472-b792ec8250ee', 'Update Task Status', 'Tasks.UpdateStatus', '881263f8-95d7-4410-88e5-d3873f99b53e', NULL, '2024-11-19T07:35:52.986557Z'),
-                    ('cdfae99c-e8f7-4a0a-9802-401cb61bc4c7', 'View All Tasks', 'Tasks.ViewAll', '881263f8-95d7-4410-88e5-d3873f99b53e', NULL, '2024-11-19T07:35:52.986430Z'),
+                    ('b8c527cf-4f12-44d2-8d8e-ca176364b9ce', 'Tasks Management', 'Tasks', NULL, NULL, '2024-11-19T07:35:52.981659Z'),
+                    ('149a06d0-0847-4f44-9d4f-00a0f16a1ada', 'Assign Task', 'Tasks.Assign', 'b8c527cf-4f12-44d2-8d8e-ca176364b9ce', NULL, '2024-11-19T07:35:52.986700Z'),
+                    ('24cf5b06-7f8b-4608-b3d6-d15d75c93b21', 'View List Tasks', 'Tasks.View', 'b8c527cf-4f12-44d2-8d8e-ca176364b9ce', NULL, '2024-11-19T07:35:52.986298Z'),
+                    ('3b28e3bc-edd7-40f0-b708-76b53023365f', 'Update Task Status', 'Tasks.UpdateStatus', 'b8c527cf-4f12-44d2-8d8e-ca176364b9ce', NULL, '2024-11-19T07:35:52.986557Z'),
+                    ('d983c04c-1730-4870-a600-ba238b854945', 'View All Tasks', 'Tasks.ViewAll', 'b8c527cf-4f12-44d2-8d8e-ca176364b9ce', NULL, '2024-11-19T07:35:52.986430Z'),
 
                     -- Children Permissions for Users
-                    ('d51e853a-4d25-4121-bec1-c4a235b3db3e', 'Users Management', 'Users', NULL, NULL, '2024-11-19T07:35:52.981763Z'),
-                    ('3686a277-2718-4f19-b061-b59ac015f786', 'View List Users', 'Users.View', 'd51e853a-4d25-4121-bec1-c4a235b3db3e', NULL, '2024-11-19T07:35:52.986832Z'),
-                    ('bf6a0114-08a8-49ec-99d2-b12b35ae71ae', 'Update User', 'Users.Update', 'd51e853a-4d25-4121-bec1-c4a235b3db3e', NULL, '2024-11-19T07:35:52.986971Z'),
+                    ('d33e6bbe-fb32-43c6-85f1-ad195539428f', 'Users Management', 'Users', NULL, NULL, '2024-11-19T07:35:52.981763Z'),
+                    ('3df1151a-bccd-41ff-9ffc-e9646e681417', 'View List Users', 'Users.View', 'd33e6bbe-fb32-43c6-85f1-ad195539428f', NULL, '2024-11-19T07:35:52.986832Z'),
+                    ('7d3d4799-4188-4f8e-ae6e-efeab1bee239', 'Update User', 'Users.Update', 'd33e6bbe-fb32-43c6-85f1-ad195539428f', NULL, '2024-11-19T07:35:52.986971Z'),
 
                     -- Children Permissions for Settings
-                    ('e8d8e99d-d6e4-4199-823e-67bef350aa1a', 'Settings Management', 'Settings', NULL, NULL, '2024-11-19T07:35:52.981898Z'),
-                    ('0865f67b-f075-4107-9044-a9c7ea9112be', 'View List Settings', 'Settings.View', 'e8d8e99d-d6e4-4199-823e-67bef350aa1a', NULL, '2024-11-19T07:35:52.987104Z'),
-                    ('43f96ebf-ebce-4927-894f-14db8232b7e6', 'Delete Setting', 'Settings.Delete', 'e8d8e99d-d6e4-4199-823e-67bef350aa1a', NULL, '2024-11-19T07:35:52.987565Z'),
-                    ('46c5ce9f-3c2e-4462-aeea-fed83c9fd1cc', 'Create Setting', 'Settings.Create', 'e8d8e99d-d6e4-4199-823e-67bef350aa1a', NULL, '2024-11-19T07:35:52.987235Z'),
-                    ('79931735-d83f-4799-b1df-36932923ba33', 'Update Setting', 'Settings.Update', 'e8d8e99d-d6e4-4199-823e-67bef350aa1a', NULL, '2024-11-19T07:35:52.987426Z'),
+                    ('55fe7d90-7cb2-4f92-8385-ec834c774cbd', 'Settings Management', 'Settings', NULL, NULL, '2024-11-19T07:35:52.981898Z'),
+                    ('129f79f2-be33-4df1-9a2f-972bc3e71500', 'View List Settings', 'Settings.View', '55fe7d90-7cb2-4f92-8385-ec834c774cbd', NULL, '2024-11-19T07:35:52.987104Z'),
+                    ('536d29fd-2c17-4012-951d-ebac875053f7', 'Delete Setting', 'Settings.Delete', '55fe7d90-7cb2-4f92-8385-ec834c774cbd', NULL, '2024-11-19T07:35:52.987565Z'),
+                    ('fde1430d-5100-4077-aa94-b85b54a422ca', 'Create Setting', 'Settings.Create', '55fe7d90-7cb2-4f92-8385-ec834c774cbd', NULL, '2024-11-19T07:35:52.987235Z'),
+                    ('6d3213be-f7d6-4675-a452-5396f200be01', 'Update Setting', 'Settings.Update', '55fe7d90-7cb2-4f92-8385-ec834c774cbd', NULL, '2024-11-19T07:35:52.987426Z'),
 
                     -- Children Permissions for WorkflowDefinitions
                     ('6734d713-cd5c-4c9d-880f-dc0e3955d8b1', 'Request Templates', 'WorkflowDefinitions', NULL, NULL, '2024-11-19T07:35:52.973059Z'),
-                    ('643d855c-8846-4586-aeb4-3299a72e2813', 'Import Request Templates', 'WorkflowDefinitions.Import', '6734d713-cd5c-4c9d-880f-dc0e3955d8b1', NULL, '2024-11-19T07:35:52.985121Z'),
+                    ('643d855c-8846-4586-aeb4-3299a72e2813', 'View List Request Templates', 'WorkflowDefinitions.View', '6734d713-cd5c-4c9d-880f-dc0e3955d8b1', NULL, '2024-11-19T07:35:52.985121Z'),
+                    ('1714e44b-8020-45af-8b66-34aa4066f51d', 'Define Input Request Templates', 'WorkflowDefinitions.DefineInput', '6734d713-cd5c-4c9d-880f-dc0e3955d8b1', NULL, '2024-11-19T07:35:52.985121Z'),
+                    ('652b036b-fc7f-4305-8d8d-1b646f88224c', 'Import Request Templates', 'WorkflowDefinitions.Import', '6734d713-cd5c-4c9d-880f-dc0e3955d8b1', NULL, '2024-11-19T07:35:52.985121Z'),
                     ('6dc377be-198f-4a81-869d-b1c8666d2706', 'Edit Request Templates', 'WorkflowDefinitions.Edit', '6734d713-cd5c-4c9d-880f-dc0e3955d8b1', NULL, '2024-11-19T07:35:52.985892Z'),
-                    ('9cefeb05-62cf-4c58-8157-5f6c82fbe9e1', 'Create Request Templates', 'WorkflowDefinitions.Create', '6734d713-cd5c-4c9d-880f-dc0e3955d8b1', NULL, '2024-11-19T07:35:52.985389Z');
+                    ('4e4e9032-19f0-49a9-8c88-93da17864104', 'Create Request Templates', 'WorkflowDefinitions.Create', '6734d713-cd5c-4c9d-880f-dc0e3955d8b1', NULL, '2024-11-19T07:35:52.985389Z'),
+                    ('497286cc-3b83-4b32-81e7-1925895fdcc4', 'Update Request Templates Status', 'WorkflowDefinitions.UpdateStatus', '6734d713-cd5c-4c9d-880f-dc0e3955d8b1', NULL, '2024-11-19T07:35:52.985389Z'),
+                    ('7a16ffd2-b817-41d9-8555-fc9645f279b2', 'Delete Request Templates', 'WorkflowDefinitions.Delete', '6734d713-cd5c-4c9d-880f-dc0e3955d8b1', NULL, '2024-11-19T07:35:52.985389Z');
                 END IF;
                 END $$;
             ");
@@ -60,7 +64,7 @@ namespace W2.Migrations
                 UPDATE ""AbpRoles""
                 SET ""Permissions"" = '[
                     {
-                    ""Id"": ""319dca97-f851-4f60-960c-3b2d201f1787"",
+                    ""Id"": ""6734d713-cd5c-4c9d-880f-dc0e3955d8b1"",
                     ""Code"": ""WorkflowDefinitions"",
                     ""Name"": ""Request Templates"",
                     ""Children"": [
@@ -71,7 +75,7 @@ namespace W2.Migrations
                         ""CreationTime"": ""2024-11-08T06:19:05.859133Z""
                         },
                         {
-                        ""Id"": ""1ce2613a-9369-4521-8a7d-0a5f1d94d515"",
+                        ""Id"": ""6dc377be-198f-4a81-869d-b1c8666d2706"",
                         ""Code"": ""WorkflowDefinitions.Edit"",
                         ""Name"": ""Edit Request Templates"",
                         ""CreationTime"": ""2024-11-08T06:19:05.859259Z""
@@ -223,19 +227,19 @@ namespace W2.Migrations
                         ""CreationTime"": ""2024-11-08T06:19:05.861446Z""
                         },
                         {
-                        ""Id"": ""ee42c1e5-6c8a-4366-bce6-9a95a7a51514"",
+                        ""Id"": ""54e8008c-6d2d-4c00-873f-39c33c3ed705"",
                         ""Code"": ""Roles.View"",
                         ""Name"": ""View List Roles"",
                         ""CreationTime"": ""2024-11-08T06:19:05.861291Z""
                         },
                         {
-                        ""Id"": ""ee42c1e5-6c8a-4366-bce6-9a95a7a51514"",
+                        ""Id"": ""e18dd7ad-ecc1-4ef6-9574-efa55f4fcd18"",
                         ""Code"": ""Roles.Delete"",
                         ""Name"": ""Delete Roles"",
                         ""CreationTime"": ""2024-11-08T06:19:05.861291Z""
                         },
                         {
-                        ""Id"": ""ee42c1e5-6c8a-4366-bce6-9a95a7a51514"",
+                        ""Id"": ""e117cd9c-2cde-4ec6-8d51-1fc37b0a282a"",
                         ""Code"": ""Roles.DeleteUserOnRole"",
                         ""Name"": ""Delete User On Role"",
                         ""CreationTime"": ""2024-11-08T06:19:05.861291Z""
