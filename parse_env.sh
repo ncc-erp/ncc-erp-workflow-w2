934ff4f9d33178805d3e38355b809ad84cf2a843
thiendangncc <72342184+thiendangncc@users.noreply.github.com>
2024-11-22 11:47:24 +0700

    Update parse_env.sh

--- parse_env.sh
+++ parse_env.sh
@@ -2,9 +2,12 @@
 
 cp appsettings_exp.json  appsettings.json 
 
+export KOMU_API_URL=$(echo "$KOMU_API_URL" | sed 's/\//\\\//g' )
 export API_URL=$(echo "$API_URL" | sed 's/\//\\\//g' )
 export URL_WEB=$(echo "$URL_WEB" | sed 's/\//\\\//g' )
 
+sed "s/KOMU_API_URL/$KOMU_API_URL/g" -i appsettings.json
+sed "s/KOMU_X_SK/$KOMU_X_SK/g" -i appsettings.json
 sed "s/API_URL/$API_URL/g" -i appsettings.json
 sed "s/DB_HOST/$DB_HOST/g" -i appsettings.json
 sed "s/DB_NAME/$DB_NAME/g" -i appsettings.json
@@ -25,4 +28,3 @@ sed "s/GG_SECRET/$GG_SECRET/g" -i appsettings.json
 sed "s/TALENT_DF/$TALENT_DF/g" -i appsettings.json
 sed "s/URL_WEB/$URL_WEB/g" -i appsettings.json
 sed "s/WFH_DEFINITIONS_ID/$WFH_DEFINITIONS_ID/g" -i appsettings.json
-
