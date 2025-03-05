b65ad0d967b51d7c3201cfdbc6f922ba7a5e1107
minh.tranchi <minh.tranchi@ncc.asia>
2025-03-05 16:23:34 +0700

    mezon oauth2 login

--- src/W2.Application.Contracts/ExternalResources/ExternalAuthDto.cs
+++ src/W2.Application.Contracts/ExternalResources/ExternalAuthDto.cs
@@ -15,4 +15,46 @@ namespace W2.ExternalResources
     {
         public string Token { get; set; }
     }
+
+    public class GetMezonOauthTokenDto
+    {
+        public string code { get; set; }
+        public string state { get; set; }
+        public string grant_type { get; set; }
+        public string redirect_uri { get; set; }
+        public string scope { get; set; }
+        public string client_id { get; set; }
+        public string client_secret { get; set; }
+    }
+
+    public class MezonOauthTokenResponse
+    {
+        public string access_token { get; set; }
+        public int expires_in { get; set; }
+        public string scope { get; set; }
+        public string token_type { get; set; }
+    }
+
+    public class MezonAuthDto
+    {
+        public string code { get; set; }
+        public string state { get; set; }
+        public string? scope { get; set; }
+    }
+
+    public class MezonAuthUserDto
+    {
+
+    public List<string> aud { get; set; }
+        public long authTime { get; set; }  
+        public long iat { get; set; }    
+        public string iss { get; set; }    
+        public long rat { get; set; }       
+        public string sub { get; set; }      
+    }
 }
+
+
+
+
+
